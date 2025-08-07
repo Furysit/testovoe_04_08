7740a8bf5c2f0b3b4ab57b09968e9ef5d70d216f
Илья Смоленский <smolenskiy20030409@mail.ru>
2025-08-07 22:50:15 +0300

    Fixed env

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Setting(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_URL: str
-    db_echo: bool
-    secret_key: str
+    db_url: str = Field(..., alias="DB_URL")
+    db_echo: bool = Field(..., alias="DB_ECHO")
+    secret_key: str = Field(..., alias="SECRET_KEY")
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
